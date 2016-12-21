96c54f934145955d481e6d122294ef43b55c220c
Yunlu Zhou <zhouyunlu0216@gmail.com>
2016-12-21 13:06:06 -0500

    Add files via upload

---
+++ myShoppingSite_query.sql
@@ -0,0 +1,42 @@
+ALTER TABLE `myShoppingSite`.`Order_item` 
+DROP FOREIGN KEY `orderToProduct`;
+ALTER TABLE `myShoppingSite`.`Order_item` 
+ADD CONSTRAINT `orderToProduct`
+  FOREIGN KEY (`Order_order_id`)
+  REFERENCES `myShoppingSite`.`orderTable` (`order_id`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+  
+ALTER TABLE `myShoppingSite`.`comment_table` 
+DROP FOREIGN KEY `FK_73c96a9a2b1e497581b0a91f69e`;
+ALTER TABLE `myShoppingSite`.`comment_table` 
+ADD CONSTRAINT `FK_73c96a9a2b1e497581b0a91f69e`
+  FOREIGN KEY (`productID`)
+  REFERENCES `myShoppingSite`.`productTable` (`productID`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+ALTER TABLE `myShoppingSite`.`comment_table` 
+DROP FOREIGN KEY `FK_a28409f5eb014ad880da9a54797`;
+ALTER TABLE `myShoppingSite`.`comment_table` 
+ADD CONSTRAINT `FK_a28409f5eb014ad880da9a54797`
+  FOREIGN KEY (`id`)
+  REFERENCES `myShoppingSite`.`usertable` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE RESTRICT;
+
+
+
+USE `myShoppingSite`;
+
+DELIMITER $$
+
+DROP TRIGGER IF EXISTS myShoppingSite.productTable_AFTER_DELETE$$
+USE `myShoppingSite`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `myShoppingSite`.`productTable_AFTER_DELETE` AFTER DELETE ON `productTable` FOR EACH ROW
+BEGIN
+SET SQL_SAFE_UPDATES = 0;
+delete from Order_item where item not in (select productID from productTable);
+END$$
+DELIMITER ;
